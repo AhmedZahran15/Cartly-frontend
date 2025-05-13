a4d636caece40e2e0df642828a3f44435e7e0653
“[Noor <nourmamoun@gmail.com>
2025-05-13 14:12:39 +0300

    adding user-profile-component

--- src/app/features/user-profile/components/user-profile-layout/user-profile-layout.component.ts
+++ src/app/features/user-profile/components/user-profile-layout/user-profile-layout.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './user-profile-layout.component.html',
   styleUrl: './user-profile-layout.component.css'
 })
