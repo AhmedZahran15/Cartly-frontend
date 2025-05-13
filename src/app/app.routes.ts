1d5a64482bf1174986a938c8bd121723e3b5b14c
“[Noor <nourmamoun@gmail.com>
2025-05-14 00:13:17 +0300

    removing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,4 @@
 import { Routes } from '@angular/router';
-import { UserProfileLayoutComponent } from './features/user-profile/components/user-profile-layout/user-profile-layout.component';
-import { OrdersComponent } from './features/user-profile/components/user-profile-layout/orders/orders.component';
-import { WishlistComponent } from './features/user-profile/components/user-profile-layout/wishlisht/wishlist/wishlist.component';
 
 export const routes: Routes = [
-    {path: 'profile',
-    component: UserProfileLayoutComponent,
-    children: [
-      { path: 'orders', component: OrdersComponent },
-      { path: 'wishlist', component: WishlistComponent}
-    ]}
 ]; 
