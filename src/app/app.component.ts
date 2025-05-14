c3d92d69cb4dc1ded2f8a55ab1556e863fe4f50c
Rouali <rewanali82@gmail.com>
2025-05-15 00:11:44 +0300

    refactor app structure: replace navbar and footer with main layout component; add routing for home, shop, collections, blog, contact, and cart pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './core/layout/navbar/navbar.component';
-import { FooterComponent } from "./core/layout/footer/footer.component";
+import { MainLayoutComponent } from "./core/layout/main-layout/main-layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,51 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import(
+        './features/home/pages/home-page/home-page.component'
+      ).then((m) => m.HomePageComponent)
+  },
+  {
+    path: 'shop',
+    loadComponent: () =>
+      import(
+        './features/products/pages/product-list-page/product-list-page.component'
+      ).then((m) => m.ProductListPageComponent)
+  },
+  {
+    path: 'collections',
+    loadComponent: () =>
+      import(
+        './features/collections/pages/collections-page/collections-page.component'
+      ).then((m) => m.CollectionsPageComponent)
+  },
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import(
+        './features/blog/pages/blog-page/blog-page.component'
+      ).then((m) => m.BlogPageComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import(
+        './features/contact/pages/contact-page/contact-page.component'
+      ).then((m) => m.ContactPageComponent)
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import(
+        './features/cart/pages/cart-page/cart-page.component'
+      ).then((m) => m.CartPageComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
