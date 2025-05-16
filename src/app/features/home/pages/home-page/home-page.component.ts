1fc816efc2c3d0841741679a34ba3966fb68f24d
Rouali <rewanali82@gmail.com>
2025-05-16 23:38:29 +0300

    add features highlight component with animations and integrate into home page

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { HeroSectionComponent } from "../../components/hero-section/hero-section.component";
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../../../core/services/product.service';
 import { BestSellersComponent } from '../../components/best-sellers/best-sellers.component';
+import { HeroSectionComponent } from './../../components/hero-section/hero-section.component';
+import { GenderCategoriesComponent } from '../../components/gender-categories/gender-categories.component';
+import { FeaturesHighlightComponent } from '../../components/features-highlight/features-highlight.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSectionComponent,BestSellersComponent],
+  imports: [BestSellersComponent ,HeroSectionComponent,GenderCategoriesComponent,FeaturesHighlightComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
- 
+  private _productService = inject(ProductService);
+  products = this._productService.getProducts();
 }
